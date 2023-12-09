9ab79399f92ef2415566e12b7f46a4c76dd25056
GustavoLR548 <gustavolr035@gmail.com>
2023-12-09 16:52:00 -0300

    feat: checkpoint update

--- addons/stopwatch/Stopwatch.gd
+++ addons/stopwatch/Stopwatch.gd
@@ -1,58 +1,107 @@
-@icon("res://addons/stopwatch/assets/stopwatch.png")
+@icon("res://addons/stopwatch/assets/stopwatch.svg")
 class_name Stopwatch
 extends Node
 
-## A utility node for counting time
+## A utility node for measuring time
 ##
 ## Use this node to measure the total amount of time elapsed. You can both reset the time and also specify to start
-## the time when the stopwatch enters the scene tree
+## the time when the stopwatch enters the scene tree.
 
-## Emitted when the stopwatch is requested to reset, returns the elapsed time before it is resetted
+## Emitted when the stopwatch is requested to reset, returns the elapsed time before it is resetted.
 signal time_resetted(time: float)
-## Emitted when the pause status is changed, returns if the stopwatch is currently paused or unpaused
+## Emitted when the pause status is changed, returns if the stopwatch is currently paused or unpaused.
 signal pause_state_changed(status: bool)
+## Emitted when a new checkpoint is added, returns the elapsed time when the checkpoint is requested.
+## and the difference between this checkpoint and the previous one
+signal new_checkpoint(checkpoint: float, diff_previous: float)
 
-@export_enum("Idle", "Physics") var process_callback : String = "Idle" ## Determines weather the processing of elapsed time is calculated during process(_process) or during physics frames(_physics_process) Obs.: It only takes effect when the node first enters the SceneTree
-@export var autostart := false ## Determines if the Stopwatch start after the node is ready or not
-@export var pause_on_reset := false ## Determines if the Stopwatch pauses when is resetted
+@export_enum("Idle", "Physics") var process_callback : String = "Idle" ## Determines weather the processing of elapsed time is calculated during process(_process) or during physics frames(_physics_process) Obs.: It only takes effect when the node first enters the SceneTree.
+@export var autostart := false ## Determines if the Stopwatch start after the node is ready or not.
+@export var pause_on_reset := false ## Determines if the Stopwatch pauses when is resetted.
 
-var elapsed_time := 0.0 : set = set_elapsed_time_in_seconds, get = get_elapsed_time_in_seconds ## amount of seconds elapsed since start
-var paused := true : set = _set_paused, get = _get_paused ## pauses the execution of the stopwatch if true
+var elapsed_time := 0.0 : set = set_elapsed_time_in_seconds, get = get_elapsed_time_in_seconds ## amount of seconds elapsed since start.
+var paused := true : set = _set_paused, get = _get_paused ## Determines if the execution of the stopwatch is paused or not.
+
+var checkpoints : Array[Array] : get = get_checkpoints_array, set = set_checkpoints_array ## An array containing all the times a checkpoint was requested. The time is kept within an Array with two positions, the first one is the elapsed time when the checkpoint was requested, and the second position contains the difference between the current checkpoint and the previous one.
 
 var __process_time_in_the_physics_engine := false
 
 func _ready():
+	
+	checkpoints = []
 	paused = not autostart
 	
 	if process_callback == "Physics":
 		__process_time_in_the_physics_engine = true
 
-## Configure if the stopwatch is paused or not
+## Get the array of checkpoints.
+func get_checkpoints_array() -> Array:
+	return checkpoints.duplicate(true)
+	
+func set_checkpoints_array(array: Array) -> void:
+	checkpoints = array
+
+## Configure if the stopwatch is paused or not.
 func _set_paused(is_paused: bool) -> void:
 	paused = is_paused
 	pause_state_changed.emit(paused)
 	set_process(not paused and not __process_time_in_the_physics_engine)
 	set_physics_process(not paused and __process_time_in_the_physics_engine)
 	
-## Toggle the stopwatch to be paused or not, depending on previous state
+## Toggle the stopwatch to be paused or not, depending on previous state.
 func toggle_pause() -> void:
 	paused = not paused
 	 
-## Checks weather the stopwatch is paused or not
+## Checks weather the stopwatch is paused or not.
 func _get_paused() -> bool:
 	return paused
 
-## Get elapsed time in seconds
+## Get elapsed time in seconds.
 func get_elapsed_time_in_seconds() -> float:
 	return elapsed_time
 	
-## Set elapsed time in seconds
+## Set elapsed time in seconds.
 func set_elapsed_time_in_seconds(new_time: float) -> void:
 	elapsed_time = new_time
 	
 ## Get the elapsed time since the stopwatch's start time and return a dictionary representing the time in hours, minutes, seconds, and milliseconds.
 func get_elapsed_time_as_dictionary() -> Dictionary:
 	return Stopwatch.get_time_dictionary_from_seconds(elapsed_time)
+	
+## Add a checkpoint to the stopwatch based on its elapsed time, returns an array 
+## with the current elapsed time, and the difference between the current elapsed time and the last
+## checkpoint. [br] [br]
+##
+## [b]Note[/b] : this function does not take into account if the stopwatch is paused or not, so keep this in mind when
+## invoking this function.
+func add_checkpoint() -> Array:
+	
+	var current_elapsed_time = elapsed_time
+
+	if checkpoints.size() == 0:
+		var time_array := [current_elapsed_time,current_elapsed_time]
+		
+		var tmp_array = checkpoints
+		tmp_array.append(time_array)
+		checkpoints = tmp_array
+		
+		new_checkpoint.emit(time_array.duplicate())
+		
+		return time_array.duplicate()
+		
+	var previous_checkpoint = checkpoints[-1]
+	
+	var previous_total_time = previous_checkpoint[0]
+	
+	var time_array := [current_elapsed_time, current_elapsed_time - previous_total_time]
+	
+	var tmp_array = checkpoints
+	tmp_array.append(time_array)
+	checkpoints = tmp_array
+
+	new_checkpoint.emit(time_array.duplicate())
+	
+	return time_array.duplicate()
 	
 ## Convert a time in seconds as a dictionary representing the time in hours, minutes, seconds, and milliseconds. [br]
 ## "hours" : stores the hours [br]
@@ -85,7 +134,7 @@ static func get_time_dictionary_from_seconds(total_time_in_seconds: float) -> Di
 		"milliseconds" : milliseconds
 	}
 
-## Formats the given time value into a string based on the specified format. the "format" is a string containing placeholders for day, hour, minute, second, and millisecond.[br] [br]
+## Formats the current elapsed time value into a string based on the specified format. the "format" is a string containing placeholders for day, hour, minute, second, and millisecond.[br] [br]
 ## Placeholders: "{dd}" for day, "{hh}" for hour, "{MM}" for minutes, "{ss}" for seconds, "{mmm}" for milliseconds.[br] [br]
 ## Returns:[br] [br]
 ##   String: The formatted time string based on the provided format.[br] [br]
@@ -100,11 +149,18 @@ static func get_time_dictionary_from_seconds(total_time_in_seconds: float) -> Di
 ##   - 00 hours,[br]
 ##   - 20 minutes,[br]
 ##   - 34 seconds,[br]
-##   - 567 milliseconds.
-func format_time(format: String) -> String:
+##   - 567 milliseconds. [br] [br]
+##
+## [b]Note:[/b] There is a static version of this function called [method Stopwatch.get_time_as_formatted_string]
+func get_elapsed_time_as_formatted_string(format: String) -> String:
+	return Stopwatch.get_time_as_formatted_string(elapsed_time, format)
+	
+## This function is a static version of the function [method Stopwatch.get_elapsed_time_as_formatted_string]. It works
+## in the same way, with the only differece is that here you have to provide the parameter [param time_in_seconds]
+static func get_time_as_formatted_string(time_in_seconds: float, format: String) -> String:
 	var formatted_time: String = ""
 	
-	var total_seconds = int(elapsed_time)
+	var total_seconds = int(time_in_seconds)
 	
 	@warning_ignore("integer_division")
 	var hours : int = total_seconds / 3600
@@ -118,7 +174,7 @@ func format_time(format: String) -> String:
 	@warning_ignore("integer_division")
 	var days : int = total_seconds / 86400
 	
-	var fractional_part := elapsed_time - int(elapsed_time)
+	var fractional_part := time_in_seconds - int(time_in_seconds)
 	
 	var milliseconds := int(fractional_part * 1000)
 	
@@ -136,15 +192,16 @@ func format_time(format: String) -> String:
 	formatted_time = format
 	
 	return formatted_time
-	
-## Reset the current elapsed time
+
+## Reset the current elapsed time.
 func reset() -> void:
 	var resulted_elapsed_time := elapsed_time
 
 	elapsed_time = 0
 	paused = pause_on_reset
+	checkpoints.clear()
 	
-	time_resetted.emit(elapsed_time)	
+	time_resetted.emit(elapsed_time)
 	
 func _process(delta: float) -> void:
 	elapsed_time += (delta * int(not __process_time_in_the_physics_engine))
--- examples/ClockTest.gd
+++ examples/ClockTest.gd
@@ -5,14 +5,24 @@ extends Control
 
 @export var start_button : Button
 
+@export var checkpoint_list : VBoxContainer
+@export var checkpoint_label : Label
+
+func _ready():
+	checkpoint_label.visible = false
+
 func _process(_delta) -> void:
-	clock_label.text = stopwatch.format_time("{MM}:{ss}:{mmm}")
+	clock_label.text = stopwatch.get_elapsed_time_as_formatted_string("{MM}:{ss}:{mmm}")
 
 func _on_start_pressed() -> void:
 	stopwatch.toggle_pause()
 
 func _on_reset_pressed() -> void:
 	stopwatch.reset()
+	checkpoint_label.visible = false
+	for i in checkpoint_list.get_children():
+		checkpoint_list.remove_child(i)
+		i.queue_free()
 
 func _on_stopwatch_pause_state_changed(stopwatch_paused: bool) -> void:
 	if stopwatch_paused and stopwatch.elapsed_time > 0:
@@ -21,3 +31,18 @@ func _on_stopwatch_pause_state_changed(stopwatch_paused: bool) -> void:
 		start_button.text = "Start"
 	else:
 		start_button.text = "Pause"
+
+func _on_lap_pressed():
+	
+	var checkpoint = stopwatch.add_checkpoint()
+	
+	var new_label = Label.new()
+	var elapsed_time = Stopwatch.get_time_as_formatted_string(checkpoint[0],"{MM}:{ss}:{mmm}")
+	var diff_time = Stopwatch.get_time_as_formatted_string(checkpoint[1],"{MM}:{ss}:{mmm}")
+	new_label.text = "%s  +%s" % [elapsed_time, diff_time]
+	new_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	
+	if checkpoint_list.get_child_count() == 0:
+		checkpoint_label.visible = true
+		
+	checkpoint_list.add_child(new_label)
